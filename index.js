05e676539c406d768501435ebf0952a5f44feb98
Pooja Patel <poojapatel17@gmail.com>
2019-04-12 18:03:21 -0500

    first assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('The new file!');
+
+var x = 'a string variable';
+console.log(x);
+
+console.log(document);
+console.log($(document));
+console.log($('div'));
+
+//console.log($);
+//$( document ).ready(function() {
+    // Handler for .ready() called.
+//    console.log('I am ready');
+//});
